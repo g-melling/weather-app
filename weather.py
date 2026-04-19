07029ac1bdd24a6bf9071b646a6c1139237d7306
Georgina Melling <156523659+georginamelling@users.noreply.github.com>
2026-04-19 11:38:15 +0100

    Air quality display added

--- weather.py
+++ weather.py
@@ -4,15 +4,17 @@ import json
 
 root = Tk()
 root.title("Weather App")
-root.geometry("400x50")
+root.geometry("400x400")
 
+# API: airnow.gov
 # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=7D99F44B-1DA2-4F75-8801-530F46CE99D9
 
-api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=7D99F44B-1DA2-4F75-8801-530F46CE99D9")
-
 try:
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=7D99F44B-1DA2-4F75-8801-530F46CE99D9")
     api = json.loads(api_request.content)
 except Exception as e:
     api = "Error..."
+    
+myLabel = Label(root, text=api[0]["Category"]["Name"]).pack()
 
 root.mainloop()
