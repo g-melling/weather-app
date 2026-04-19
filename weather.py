614498a054dc48056ae464c03958cacdd42d4ce7
Georgina Melling <156523659+georginamelling@users.noreply.github.com>
2026-04-19 11:42:58 +0100

    City/ AQI/ Quality output

--- weather.py
+++ weather.py
@@ -12,9 +12,14 @@ root.geometry("400x400")
 try:
     api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=7D99F44B-1DA2-4F75-8801-530F46CE99D9")
     api = json.loads(api_request.content)
+    city = text=api[0]["ReportingArea"]
+    quality = text=api[0]["AQI"]
+    category = text=api[0]["Category"]["Name"]
 except Exception as e:
     api = "Error..."
     
-myLabel = Label(root, text=api[0]["Category"]["Name"]).pack()
+output = f"City: {city}\nAir Quality Index: {quality}\nCategory: {category}"
+    
+myLabel = Label(root, text=output).pack()
 
 root.mainloop()
